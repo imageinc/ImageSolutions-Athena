3d13c9945e22375a48172ee600647177bd27c978
Vincent Lee <li.vincent@gmail.com>
2024-10-23 15:10:04 -0700

    first version

---
+++ AthenaWebSite/App_Code/BaseControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AthenaWebSite
+{
+    public class BaseControl : System.Web.UI.UserControl
+    {
+        public BasePage ThisPage
+        {
+            get
+            {
+                if (this.Page is BasePage)
+                    return (BasePage)this.Page;
+                else
+                    throw new Exception("Page containing this user control must inherit from BasePage");
+            }
+        }
+    }
+}
