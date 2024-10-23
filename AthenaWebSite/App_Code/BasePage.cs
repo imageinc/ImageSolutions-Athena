2697b8e476c600ab849fd590c2753b48d3178fba
Vincent Lee <li.vincent@gmail.com>
2024-10-23 15:10:04 -0700

    first version

---
+++ AthenaWebSite/App_Code/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI.HtmlControls;
+
+namespace AthenaWebSite
+{
+    public class BasePage : System.Web.UI.Page
+    {
+    }
+}
